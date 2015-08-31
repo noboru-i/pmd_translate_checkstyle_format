7f9385df125bfedf3f4cde9d87252746664cf07e
noboru-i <ishikura.noboru@gmail.com>
2015-08-31 17:15:21 +0900

    remove spec

--- spec/pmd_translate_checkstyle_format_spec.rb
+++ spec/pmd_translate_checkstyle_format_spec.rb
@@ -4,8 +4,4 @@ describe PmdTranslateCheckstyleFormat do
   it 'has a version number' do
     expect(PmdTranslateCheckstyleFormat::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
